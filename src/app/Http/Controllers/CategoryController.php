ea249d7f87e7ee5912559ffe2745a8cb0f89fb55
misa <mmichiura33@gmail.com>
2025-02-28 22:47:09 +0900

    レイアウトファイル(app.blade.php)と共通スタイル(common.css)を作成

---
+++ src/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    
+}
