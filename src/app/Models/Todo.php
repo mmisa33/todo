e5fff4ebb323f145cb386667d5fc7f7302922132
misa <mmichiura33@gmail.com>
2025-03-01 08:34:14 +0900

    Todoのマイグレーションファイルとモデルを作成

--- src/app/Models/Todo.php
+++ src/app/Models/Todo.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use LDAP\Result;
 
 class Todo extends Model
 {
     use HasFactory;
 
-
+    protected $fillable = ['content'];
 }
